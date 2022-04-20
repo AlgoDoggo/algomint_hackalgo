456ba9acac5e146d6dca0a62b81cec60156f46e7
AlgoDoggo <algodoggos@gmail.com>
2022-04-20 18:17:53 +0200

    fix python file path

--- src/helpers/getTinyLSig.ts
+++ src/helpers/getTinyLSig.ts
@@ -1,14 +1,13 @@
 import { LogicSigAccount } from "algosdk";
 import { spawn } from "child_process";
+import { fileURLToPath } from "url";
 import { tinyValidatorApp } from "../constants/constants.js";
 
 const getTinyLSig = async (assets: number[]): Promise<LogicSigAccount> => {
-  const python = spawn("python", [
-    `src/utils/tinyPoolGen.py`,
-    tinyValidatorApp.toString(),
-    assets[0].toString(),
-    assets[1].toString(),
-  ]);
+  const url = new URL("../utils/tinyPoolGen.py", import.meta.url);
+  const file = fileURLToPath(url);
+
+  const python = spawn("python", [file, tinyValidatorApp.toString(), assets[0].toString(), assets[1].toString()]);
 
   const lsig: LogicSigAccount = await new Promise((resolve, reject) => {
     python.stdout.on("data", (data) => {
@@ -24,4 +23,4 @@ const getTinyLSig = async (assets: number[]): Promise<LogicSigAccount> => {
 
   return lsig;
 };
-export default getTinyLSig
+export default getTinyLSig;
